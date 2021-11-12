3d69dc32c318967442404da27d597605a89d7a34
alexpls2021 <alexpls@live.com>
2021-11-12 15:13:03 -0300

    Clean code app

--- app/app.py
+++ app/app.py
@@ -26,64 +26,6 @@ def index():
     return render_template('index.html', posts=posts)
 
 
-def get_post(post_id):
-   post = Posts.query.filter_by(id=post_id).first()
-   if post is None:
-      abort(404)
-   return post
-
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
-
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('O titulo é obrigatório!')
-        else:
-            post = Posts(title=title, content=content)
-            db.session.add(post)
-            db.session.commit()
-            return redirect(url_for('index'))
-
-    return render_template('create.html')
-
-
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Titulo é obrigatório!')
-        else:
-            post.title = title
-            post.content = content
-            db.session.commit()
-            return redirect(url_for('index'))
-
-    return render_template('edit.html', post=post)
-
-
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    db.session.delete(post)
-    db.session.commit()
-    flash('"{}" foi apagado com sucesso!'.format(post.title))
-    return redirect(url_for('index'))
-
-
 @app.route('/responsaveis')
 def responsaveis():
     return render_template('responsaveis.html')
@@ -97,6 +39,3 @@ def escola():
 @app.route('/transportado')
 def transportado():
     return render_template('transportado.html')
-
-
-
