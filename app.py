ebeb734057d0658c81de7039c968f84e42ecc6a0
alexpls2021 <92728746+alexpls2021@users.noreply.github.com>
2022-09-26 15:31:12 -0300

    Removed comment

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ def cadastro():
         return redirect("/#header")
     return render_template("cadastro.html", active='cadastro')
 
- # Rota da consulta
 @app.route("/consultar")
 def consultar():
     pessoas = Pessoas.query.all()
